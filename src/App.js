210279399107cf27f81c3efca55125ca497cd034
Anubhav Choudhary <ac.10edu@gmail.com>
2022-01-15 20:24:50 +0530

    Connecting with Phantom wallet.
    - `solana` window object, containing crypt address from phantom
    
    Signed-off-by: Anubhav Choudhary <ac.10edu@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useState } from "react";
 
-function App() {
+const App = () => {
+  const [walletConnected, setWalletConnected] = useState(false);
+  const [provider, setProvider] = useState();
+  const [loading, setLoading] = useState();
+
+  const getProvider = async () => {
+    if ("solana" in window) {
+      const provider = window.solana;
+      if (provider.isPhantom) {
+        return provider;
+      }
+    } else {
+      window.open("https://www.phantom.app/", "_blank");
+    }
+  };
+
+  const walletConnectionHelper = async () => {
+    if (walletConnected) {
+      //Disconnect Wallet
+      setProvider();
+      setWalletConnected(false);
+    } else {
+      const userWallet = await getProvider();
+      if (userWallet) {
+        await userWallet.connect();
+        userWallet.on("connect", async () => {
+          setProvider(userWallet);
+          setWalletConnected(true);
+        });
+      }
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div>
+      <h1>Create your own token using JavaScript</h1>
+      {walletConnected ? (
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <strong>Public Key:</strong> {provider.publicKey.toString()}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      ) : (
+        <p></p>
+      )}
+
+      <button onClick={walletConnectionHelper} disabled={loading}>
+        {!walletConnected ? "Connect Wallet" : "Disconnect Wallet"}
+      </button>
     </div>
   );
-}
+};
 
 export default App;
